bc2e20889e5c160ecac57ed61a9b51e08abb9472
HannaHutt <huttmannhanna@gmail.com>
2022-08-16 23:29:10 -0300

    Add avance

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,26 @@ import './App.css'
 import { ChakraProvider, Heading } from '@chakra-ui/react'
 import { Navbar } from './Components/Navbar'
 import {ItemListContainer} from './Components/ItemListContainer'
+import { ItemDetailContainer } from "./Components/ItemDetailContainer"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <ChakraProvider>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+
+       <Route path="/" element={<ItemListContainer greeting="Bienvenidos a Hanna Baking"  />} />
+       <Route path="/category/:category" element={<ItemListContainer greeting= "Bienvenidos a Hanna Baking" /> } />
+       <Route path="/product/:id" element={<ItemDetailContainer /> } />
+
+      </Routes>
+       
       
-       <Navbar />
-       <ItemListContainer greeting="Bienvenidos a Hanna Baking"  />
+      </BrowserRouter>
+      
+       
     </ChakraProvider>
   )
 }
---
+++ src/Components/ItemDetail/index.jsx
@@ -0,0 +1,7 @@
+const ItemDetail = ({listProduct}) => {
+     
+    return(
+        <div>{listProduct.product}</div>
+    )
+}
+export {ItemDetail}
